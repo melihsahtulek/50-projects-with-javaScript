41c71e8fe5ca0cc77bac5402a35b661d9a197c9e
melihsah t <melihsahtulek@gmail.com>
2022-08-10 02:02:13 +0300

    finished project 4

--- scroll-animation-box/assets/js/main.js
+++ scroll-animation-box/assets/js/main.js
@@ -1,3 +1,36 @@
 window.addEventListener("load", () => {
-  console.log(window.screen.width / 2);
+  const boxes = document.querySelectorAll(".animated-box");
+
+  // TRANSLATE ALL ELEMENTS
+  boxes.forEach((box, index) => {
+    let documentW = document.body.clientWidth;
+    let leftW = box.getBoundingClientRect().left;
+    let boxW = box.offsetWidth;
+    let dynamicW = documentW - leftW - boxW;
+    if (index % 2 === 0) {
+      box.style.transform = `translateX(${dynamicW + boxW}px)`;
+      box.setAttribute("data-value", dynamicW + boxW);
+    } else {
+      box.style.transform = `translateX(-${leftW + boxW}px)`;
+      box.setAttribute("data-value", (leftW + boxW) * -1);
+    }
+  });
+
+  const showHide = () => {
+    boxes.forEach((box) => {
+      if (window.innerHeight > box.getBoundingClientRect().top + 100) {
+        box.style.transform = "translateX(0)";
+      } else {
+        box.style.transform = `translateX(${box.getAttribute("data-value")}px)`;
+      }
+      box.style.transition = "all .25s";
+    });
+  };
+
+  // SCROLL EVENT
+  window.addEventListener("scroll", (e) => {
+    showHide();
+  });
+
+  showHide();
 });
